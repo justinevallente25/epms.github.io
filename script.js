e275471ceb71feb5c951c66128ccf88f1244eeb8
justinevallente25 <60265425+justinevallente25@users.noreply.github.com>
2024-09-20 08:13:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add interactivity for download button, scroll effects, or more
+document.querySelector('.download-btn').addEventListener('click', function() {
+    alert('Downloading mobile app...');
+  });
+  
